8c7af6b7733679e63b171c6add416e6087e8e7fd
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-12-04 20:20:23 +0500

    Add exercise code

---
+++ age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you will be:"
+puts age + 10
+puts "In 20 years you will be:"
+puts age + 20
+puts "In 30 years you will be:"
+puts age + 30
+puts "In 40 years you will be:"
+puts age + 40
---
+++ name.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "Hello " + name
+
+10.times do
+  puts name
+end
